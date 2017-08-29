b7d4c3bd291e7845f6ffb1ca69ae4f4aa256dcbb
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2017-08-29 11:44:14 +0300

    public_wt_url removed

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ module.exports = (configProvider, storageProvider) => {
     audience: 'urn:logs-to-loggly',
     rta: config('AUTH0_RTA').replace('https://', ''),
     domain: config('AUTH0_DOMAIN'),
-    baseUrl: config('PUBLIC_WT_URL') || config('WT_URL'),
+    baseUrl: config('WT_URL'),
     clientName: 'Logs to Loggly',
     urlPrefix: '',
     sessionStorageKey: 'logs-to-loggly:apiToken'
